898382f1525f4f2dcbae337e2ccf8d7ea4f16eb6
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-20 19:13:25 -0800

    Created new project. Added inceptionv3.mimodel (google's model)
    Started working on the camara functionality.

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by luis salazar on 12/20/18.
+//  Copyright © 2018 F Productions. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        //implementing camera property
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
